eb4d24e50fc026eb9e5bb5cf15f0a0238a67ef8a
katiehrenchir <katie.hrenchir@gmail.com>
2019-05-09 01:20:20 -0500

    Adds query to see pet names

--- php/ViewPets.php
+++ php/ViewPets.php
@@ -1,49 +1,27 @@
 <?php
 
+include 'connect.php';
+
+echo "<link rel='stylesheet' type='text/css' href='../css/style.css' />"; 
+
 $mysqli = OpenCon();
-echo "connected successfully";
+echo "<h1>connected successfully</h1>";
+
+$query = "SELECT NAME FROM PET";
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        printf ("%s (%s)\n", $row["Name"]);
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+
 closeCon($mysqli)
 
-// $petname = $_POST['name'];
-// echo "<h1> Information about ".$petname."</h1>";
-
-// $postId = array();
-// $postContent = array();
-// $query = "SELECT Posts.author_id, Posts.post_id, Posts.content FROM Users INNER JOIN Posts ON Users.user_id=Posts.author_id ORDER BY Posts.post_id";
-// $length = 0;
-// if ($result = $mysqli->query($query)) {
-
-//     /* fetch associative array */
-
-// 	while ($row = $result->fetch_assoc()) {
-// 		if(strcasecmp($row["author_id"], $username) == 0 ) {
-// 			$postContent[$length] = $row["content"];
-// 			$postId[$length] = $row["post_id"];
-// 			$length++;
-// 		}
-// 	}
-
-
-//     /* free result set */
-//     $result->free();
-// } 
-
-
-// $arrlength = count($postId);
-// if($arrlength != 0)  {
-// 	echo "<table><tr><th>Post Id</th><th>Post Content</th></tr>";
-// 	for($x = 0; $x < $arrlength; $x++) {
-// 		echo "<tr><td>".$postId[$x]."</td>";
-// 		echo "<td>".$postContent[$x]."</td></tr>";
-// 	}
-// 	echo "</table>";
-// } else {
-// 	echo "There is no information about this pet.";
-// }
-
-// 	echo " <br>";
-// 	echo "<a href=\"../src/AdminHome.html\" class=\"button\">Return to Search Page</a>";
-// /* close connection */
-// $mysqli->close();
 
 ?>  
