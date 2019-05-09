baa85778358047a71d150c46ce92c17ad4c12d49
katiehrenchir <katie.hrenchir@gmail.com>
2019-05-09 01:23:13 -0500

    why is this failing fml

--- php/ViewPets.php
+++ php/ViewPets.php
@@ -1,10 +1,18 @@
 <?php
 
-include 'connect.php';
-
 echo "<link rel='stylesheet' type='text/css' href='../css/style.css' />"; 
 
-$mysqli = OpenCon();
+include("credentials.php");
+$password = PASSWORD
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "hkathleen", $password, "hkathleen");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
 echo "<h1>connected successfully</h1>";
 
 $query = "SELECT NAME FROM PET";
@@ -20,8 +28,7 @@ if ($result = $mysqli->query($query)) {
     $result->free();
 }
 
-
-closeCon($mysqli)
-
+/* close connection */
+$mysqli->close();
 
 ?>  
